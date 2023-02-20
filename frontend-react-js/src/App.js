1212ce477e159edc2dfb678f8f594e53d7254846
Cristian Ramos <ramofabian@gmail.com>
2023-02-20 21:13:37 +0000

    Doumentin openapi, write endpoint and write a page

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
+import NotificationsActivities from './pages/NotificationsActivities';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeFeedPage />
+  },
+  {
+    path: "/notifications",
+    element: <NotificationsActivities />
   },
   {
     path: "/@:handle",
